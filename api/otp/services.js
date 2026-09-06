b758fa9e83585c9c8ef8ce2bd21a27df98e83006
NanoXyin Official <vanr7gaming@gmail.com>
2026-09-06 21:48:56 +0700

    Delete api/otp/services.js

--- api/otp/services.js
+++
@@ -1,7 +0,0 @@
-module.exports=async(req,res)=>{
-  try{
-    const r=await fetch("https://www.rumahotp.io/api/v2/services",{headers:{"x-apikey":process.env.RUMAHOTP_API_KEY,"Accept":"application/json"}});
-    const data=await r.json();
-    res.status(r.status).json(data);
-  }catch(e){res.status(502).json({success:false,error:{message:"Upstream unavailable"}})}
-};
